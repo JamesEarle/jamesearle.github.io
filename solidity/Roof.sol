208f73d640b4b66b01dbb3e9551b2af8b591621a
James Earle <JamesEarle@users.noreply.github.com>
2021-03-20 14:36:33 -0700

    update to roof contract

--- solidity/Roof.sol
+++ solidity/Roof.sol
@@ -1,65 +1,70 @@
 //SPDX-License-Identifier: UNLICENSED
-pragma solidity >0.7.0 <=0.9.0;
+pragma solidity >0.7.0 <=0.9.0; 
 pragma experimental ABIEncoderV2;
 // import "https://github.com/JamesEarle/jamesearle.github.io/blob/main/solidity/IChargedParticleBroker.sol";
 import "./IRoof.sol";
-import ".IChargedParticleBroker.sol"
+// import ".IChargedParticleBroker.sol";
 
-contract Roof is IRoof {
+contract Roof { // 
     
-    // IChargedParticleBroker Broker; 
-    address indexed tenant;
-    address indexed landlord;
-
-    event SetOwner (address _owner)
-
-    modifier isOwner () {
-        require(msg.sender == owner, "Caller is not owner");
-        _;
-    }
-
+    address private tenant;
+    address private landlord;
+    
     struct Lease {
-        address indexed tenant;
-        address indexed landlord;
+        address tenant;
+        address landlord;
         uint256 rent;
         uint256 deposit;
-        string duration;
+        uint256 durationMonths;
     }
-
-    function createRental (address _tenant, address _landlord) external override returns (string memory _accesshash) {
+    
+    constructor() {
+        // Use MetaMask sample test account addresses
+        tenant = 0x955A52f4F067cB1B5eC7B136995f93A82d3eb3bD;
+        landlord = 0xF920f5E23983069CF36793E0E95F444fC4f4d388;
+    }
+    
+    function setTenantAddress (address _tenant) public {
+        tenant = _tenant;
+    }
+    
+    function setLandlordAddress (address _landlord) public {
+        landlord = _landlord;
+    }
+    
+    function bytes32ToString(bytes32 _data) public returns (string memory) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(_data) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (uint j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
+    
+    function createRental (uint256 _rent, uint256 _deposit, uint256 _durationMonths) public returns (string memory _accessHash) {
         //take the rental info and submit the right data to create a lease deposit
+        Lease memory myLease = Lease({
+            tenant: tenant,
+            landlord: landlord,
+            rent: _rent,
+            deposit: _deposit,
+            durationMonths: _durationMonths
+        });
+        
+        bytes32 hash = keccak256(abi.encode(myLease));
+        string memory accessHash = bytes32ToString(hash);
+
+        return accessHash;
     }
     
-    function endRental () external override returns (uint256 _depositAmount, uint256 _interestAmount){
-        //Use existing data and call closeleasedeposit
-    }
-    
-    function payRent () external override returns (uint256 _paymentReference){
-        //using existing data call createrentdeposit 
-    }
-    
-    function getEarningsData () external override returns (uint256 [] memory _depositAmount, uint256 [] memory _interestAccrued, uint256 [] memory _retrievalDate){
-        //using existing data call getinterestondeposit
-    }
-    
-    function giveNotice () external override returns (uint256 _noticeReference, uint256 _date){
-        //purely a roof operation using existing data
-    }
-    
-    function getLandlordLeaseData () external override returns (string [] memory _leaseID, uint256 [] memory _leaseStartDate, uint256 [] memory _leaseEndDate, uint256 [] memory _rentalAmount, uint256 [] memory _depositAmount, string [] memory _rentPaymentStatus, string [] memory _leaseStatus){
-        //using existing data call getinterestonrent
-    }
-    
-    function getLeaseDetail () external override returns (string memory _leaseID, string memory _address, string memory _tenantReference, uint256 _noticeReference, uint256 _noticePeriod, uint256 _depositPayDate, uint256 _rentPayDate, uint256 _leaseStartDate, uint256 _leaseEndDate){
-        //using existing data
-    }
-    
-    function payDeposit () external override returns (uint256 _paymentReference){
-        //using existing data call createdeposit
-    }
-    
-    function setInterestPayoutPreference () external override returns (uint256 _date){
-        //internal roof operation
-    }  
     
 }
